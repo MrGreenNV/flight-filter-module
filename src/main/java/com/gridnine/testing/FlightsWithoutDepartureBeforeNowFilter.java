6b60a418931b9985b8c3133ba31e3195cc645ca4
MrGreenNV <averkievnv@gmail.com>
2023-08-28 22:27:53 +0300

    add FlightsWithoutDepartureBeforeNowFilter.javaWith

---
+++ src/main/java/com/gridnine/testing/FlightsWithoutDepartureBeforeNowFilter.java
@@ -0,0 +1,25 @@
+package com.gridnine.testing;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * The class is a filtering strategy that excludes flights with a departure date up to the current time.
+ * @author mrGreenNV
+ */
+public class FlightsWithoutDepartureBeforeNowFilter implements FilterStrategy {
+
+    /**
+     * Filters the list of flights, excluding flights with departure date up to the current time.
+     * @param flights list of Flight objects to filter.
+     * @return filtered list of Flight object.
+     */
+    @Override
+    public List<Flight> filter(List<Flight> flights) {
+        return flights.stream()
+                .filter(flight -> flight.getSegments().stream()
+                        .allMatch(segment -> segment.getDepartureDate().isAfter(LocalDateTime.now())))
+                .collect(Collectors.toList());
+    }
+}
