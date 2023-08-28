a45ae8a7aa670d16d91428f02bc6de7f01276095
MrGreenNV <averkievnv@gmail.com>
2023-08-28 22:23:26 +0300

    update descriptions Flight.java, FlightBuilder.java, Segment.java

--- src/main/java/com/gridnine/testing/Flight.java
+++ src/main/java/com/gridnine/testing/Flight.java
@@ -5,19 +5,34 @@ import java.util.stream.Collectors;
 
 /**
  * Bean that represents a flight.
+ * Represents a flight consisting of multiple segments.
  * @author Gridnine Systems.
  */
 class Flight {
+
+    /** List of Segment object. */
     private final List<Segment> segments;
 
+    /**
+     * Constructs a new Flight the specified segments.
+     * @param segs list of Segment object.
+     */
     Flight(final List<Segment> segs) {
         segments = segs;
     }
 
+    /**
+     * Get the list of Segment object for this flight.
+     * @return The list of Segment object.
+     */
     List<Segment> getSegments() {
         return segments;
     }
 
+    /**
+     * Returns a string representation of the flight.
+     * @return A string containing a representation of all segments in the flight.
+     */
     @Override
     public String toString() {
         return segments.stream().map(Object::toString)
--- src/main/java/com/gridnine/testing/Segment.java
+++ src/main/java/com/gridnine/testing/Segment.java
@@ -6,26 +6,51 @@ import java.util.Objects;
 
 /**
  * Bean that represents a flight segment.
+ * This class represents a single segment of a flight, characterized by its departure and arrival dates.
+ * Instances of this class are immutable once created.
  * @author Gridnine Systems.
  */
 class Segment {
+
+    /** Departure date */
     private final LocalDateTime departureDate;
 
+    /** Arrival date */
     private final LocalDateTime arrivalDate;
 
+    /**
+     * Constructs a new Segment with the specified departure and arrival dates.
+     * @param dep The departure date and time of the segment.
+     * @param arr The arrival date and time of the segment.
+     * @throws NullPointerException If either dep or arr is null.
+     */
     Segment(final LocalDateTime dep, final LocalDateTime arr) {
         departureDate = Objects.requireNonNull(dep);
         arrivalDate = Objects.requireNonNull(arr);
     }
 
+    /**
+     * Get the departure date and time of the segment.
+     * @return The departure date and time.
+     */
     LocalDateTime getDepartureDate() {
         return departureDate;
     }
 
+    /**
+     * Get the arrival date and time of the segment.
+     * @return The arrival date and time.
+     */
     LocalDateTime getArrivalDate() {
         return arrivalDate;
     }
 
+    /**
+     * Returns a string representation of the segment.
+     * The format of the string is "[departureDate|arrivalDate]" where departureDate and arrivalDate are
+     * formatted as "yyyy-MM-dd'T'HH:mm".
+     * @return The string representation of the segment.
+     */
     @Override
     public String toString() {
         DateTimeFormatter fmt =
